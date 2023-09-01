f3a0c0801bf73e14189bd35d0c94483fb2ff1a80
Miquéias Garcia <mglucenalucena@gmail.com>
2023-09-01 16:02:31 -0300

    Adicionando logo do aplicativo

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -60,9 +60,26 @@ class Navigation extends StatelessWidget {
       );
 
   Widget buildHeader(BuildContext context) => Container(
+        color: Theme.of(context).colorScheme.inversePrimary,
         padding: EdgeInsets.only(
           top: MediaQuery.of(context).padding.top,
         ),
+        child: Column(children: [
+          const CircleAvatar(
+            radius: 40,
+            backgroundImage: ExactAssetImage('assets/images/Logo.png'),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Text(
+            'U-translator',
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+        ]),
       );
 
   Widget buildMenuItems(BuildContext context) => Container(
