8774251be6111372e3f464790052501f37352349
Miquéias Garcia <mglucenalucena@gmail.com>
2023-09-15 11:56:02 -0300

    Adicionando tela de histórico na home

--- lib/controllers/home_page_body_controller.dart
+++ lib/controllers/home_page_body_controller.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:utranslator/builders/build_inicial_home_text.dart';
 import 'package:utranslator/builders/build_pdf_history.dart';
 import 'package:utranslator/builders/build_pdf_viewer.dart';
 
 class HomePageBodyController extends ChangeNotifier {
   bool buttonIsActive = true;
-  Widget currentBody = HomePageInitialText();
+  Widget currentBody = const Text("Erro ao carregar tela");
+
+  static const _key = 'if_pdf_is_already_open';
+
+  Future<void> getIfThePDFHasAlreadyBeenOpen() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isAlreadyOpen = prefs.getBool(_key) ?? false;
+    if (!isAlreadyOpen) {
+      currentBody = const HomePageInitialText();
+      notifyListeners();
+    } else {
+      currentBody = const PdfHistory();
+      notifyListeners();
+    }
+  }
+
+  Future<void> setIfThePDFHasAlreadyBeenOpen(bool isOpen) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setBool(_key, isOpen);
+  }
 
   changeBody(int index, String? pdfPath) {
     if (index == 0) {
@@ -14,11 +35,49 @@ class HomePageBodyController extends ChangeNotifier {
     } else if (index == 1 && pdfPath != null) {
       currentBody = PDFViewerBody(pdfPath: pdfPath);
       buttonIsActive = false;
+      setIfThePDFHasAlreadyBeenOpen(true);
       notifyListeners();
     } else if (index == 2) {
-      currentBody = PdfHistory();
+      currentBody = const PdfHistory();
       buttonIsActive = false;
       notifyListeners();
     }
   }
 }
+
+// body: FutureBuilder<bool>(
+//           future: IfPDFIsAlreadyOpen.getIfPDFIsAlreadyOpen(),
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//               return CircularProgressIndicator();
+//             } else if (snapshot.hasError) {
+//               return Text(
+//                   'Erro ao carregar estado da página: ${snapshot.error}');
+//             } else if (!snapshot.hasData) {
+//               return Text('Nenhum dado de estado.');
+//             } else {
+//               if (snapshot.data!) {
+//                 controller.changeBody(2, null);
+//               } else {
+//                 controller.changeBody(0, null);
+//               }
+//             }
+//             return Scaffold (
+
+// class IfPDFIsAlreadyOpen {
+//   static const _key = 'if_pdf_is_already_open';
+
+//   static Future<bool> getIfPDFIsAlreadyOpen() async {
+//     final prefs = await SharedPreferences.getInstance();
+//     final isAlreadyOpen = prefs.getBool(_key) ?? false;
+//     return isAlreadyOpen;
+//   }
+
+//   static Future<bool> addIfPDFIsAlreadyOpen(bool isOpen) async {
+//     final prefs = await SharedPreferences.getInstance();
+
+//     await prefs.setBool(_key, isOpen);
+
+//     return isOpen;
+//   }
+// }
--- lib/pages/home_page_body.dart
+++ lib/pages/home_page_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:utranslator/builders/build_floating_file_button.dart';
 import 'package:utranslator/controllers/home_page_body_controller.dart';
 
@@ -19,6 +18,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    controller.getIfThePDFHasAlreadyBeenOpen();
     controller.addListener(() {
       setState(() {});
     });
@@ -32,46 +32,9 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      body: FutureBuilder<bool>(
-          future: IfPDFIsAlreadyOpen.getIfPDFIsAlreadyOpen(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text(
-                  'Erro ao carregar estado da página: ${snapshot.error}');
-            } else if (!snapshot.hasData) {
-              return Text('Nenhum dado de estado.');
-            } else {
-              if (snapshot.data!) {
-                controller.changeBody(2, null);
-              } else {
-                controller.changeBody(0, null);
-              }
-            }
-            return Scaffold (
-              body: currentBody,
-              floatingActionButton: buttonIsActive
-                  ? FloatingFileButton(callbackFunction: callback)
-                  : null,
-            );
-          }));
-}
-
-class IfPDFIsAlreadyOpen {
-  static const _key = 'if_pdf_is_already_open';
-
-  static Future<bool> getIfPDFIsAlreadyOpen() async {
-    final prefs = await SharedPreferences.getInstance();
-    final isAlreadyOpen = prefs.getBool(_key) ?? false;
-    return isAlreadyOpen;
-  }
-
-  static Future<bool> addIfPDFIsAlreadyOpen(bool isOpen) async {
-    final prefs = await SharedPreferences.getInstance();
-
-    await prefs.setBool(_key, isOpen);
-
-    return isOpen;
-  }
+        body: currentBody,
+        floatingActionButton: buttonIsActive
+            ? FloatingFileButton(callbackFunction: callback)
+            : null,
+      );
 }
