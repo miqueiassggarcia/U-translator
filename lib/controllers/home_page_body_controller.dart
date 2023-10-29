3fc6d85833fce52ac0aabcecb287febea2086479
Miquéias Garcia <mglucenalucena@gmail.com>
2023-10-29 15:49:12 -0300

    Adicionando botão de sair do pdf e mantendo estado do pdf aberto

--- lib/controllers/home_page_body_controller.dart
+++ lib/controllers/home_page_body_controller.dart
@@ -9,11 +9,13 @@ class HomePageBodyController extends ChangeNotifier {
   bool buttonIsActive = true;
   Widget currentBody = Container();
 
-  static const _key = 'if_pdf_is_already_open';
+  static const _keyIfOpen = 'if_pdf_is_already_open';
+  static const _keyPDFOpen = 'pdf_is_open';
 
-  Future<void> changeIfThePDFHasAlreadyBeenOpen(Function callbackFunction) async {
+  Future<void> changeIfThePDFHasAlreadyBeenOpen(
+      Function callbackFunction) async {
     final prefs = await SharedPreferences.getInstance();
-    final isAlreadyOpen = prefs.getBool(_key) ?? false;
+    final isAlreadyOpen = prefs.getBool(_keyIfOpen) ?? false;
     if (!isAlreadyOpen) {
       currentBody = const HomePageInitialText();
       notifyListeners();
@@ -28,14 +30,30 @@ class HomePageBodyController extends ChangeNotifier {
   Future<void> setIfThePDFHasAlreadyBeenOpen(bool isOpen) async {
     final prefs = await SharedPreferences.getInstance();
 
-    await prefs.setBool(_key, isOpen);
+    await prefs.setBool(_keyIfOpen, isOpen);
   }
 
   changeBodyToPdfView(String pdfPath) {
-    print(pdfPath);
-    currentBody = PDFViewerBody(pdfPath: pdfPath);
+    currentBody =
+        PDFViewerBody(pdfPath: pdfPath, callbackFunction: setPDFOpen);
     buttonIsActive = false;
     setIfThePDFHasAlreadyBeenOpen(true);
+    setPDFOpen(pdfPath);
     notifyListeners();
+  }
+
+  Future<void> setPDFOpen(String path) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString(_keyPDFOpen, path);
+  }
+
+  Future<void> changeIfPDFOpen() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? path = prefs.getString(_keyPDFOpen);
+
+    if (path != null && path.isNotEmpty) {
+      changeBodyToPdfView(path);
+    }
   }
 }
