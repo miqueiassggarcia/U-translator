d71806fa63e76e2702a813689e68ab4d65bce793
Miquéias Garcia <59172718+miqueiassggarcia@users.noreply.github.com>
2023-10-29 15:29:25 -0300

    criação da base de about page (#18)
    Co-authored-by: roxy22788 <roxy2298gm@gmail.com>

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -12,5 +12,49 @@ class AboutPage extends StatelessWidget {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           centerTitle: true,
         ),
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: ListView(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 50,),
+              Text("Sobre o Aplicativo de Leitura e Tradução de PDF", 
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18), 
+                  textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20,),
+              Text("Bem-vindo ao nosso aplicativo de Leitura e Tradução de PDF! " +
+                "Estamos entusiasmados por você ter escolhido nossa plataforma como " +
+                "uma ferramenta valiosa para seu aprendizado de idiomas.",
+                textAlign: TextAlign.center,),
+
+              SizedBox(height: 100,),
+              Text("Nossa Missão",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18), 
+                textAlign: TextAlign.center),
+              SizedBox(height: 20,),
+              Text("Nosso objetivo é proporcionar uma maneira eficaz e envolvente de estudar " +
+                "um novo idioma. Reconhecemos a importância de ler textos autênticos para " +
+                "aprimorar suas habilidades de idioma, e é por isso que projetamos este aplicativo.",
+                textAlign: TextAlign.center,),
+
+              SizedBox(height: 100,),
+              Text("Nossa Equipe",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18), 
+                textAlign: TextAlign.center),
+              SizedBox(height: 20,),
+              Text("Somos uma equipe apaixonada por idiomas e educação. "+
+                "Estamos comprometidos em fornecer a você as ferramentas de " +
+                "que você precisa para se tornar fluente em um novo idioma.",
+                textAlign: TextAlign.center,),
+              Text("Obrigado por escolher nosso aplicativo. Esperamos que ele seja " +
+                "uma parte valiosa do seu processo de aprendizado. Se você tiver alguma dúvida, " +
+                "comentário ou sugestão, não hesite em entrar em contato conosco.",
+                textAlign: TextAlign.center,)
+            ],
+          ),
+        ),
       );
 }
